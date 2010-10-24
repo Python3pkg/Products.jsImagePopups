617bdea6a39818aa8746f4c4fd987c4bc1e00d28
Simon Käser <skaeser@raptus.com>
2010-10-24 12:57:44 +0000

    set zip_safe flag to False and prepared for release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0b3'
+version = '2.0b4'
 
 setup(name='Products.jsImagePopups',
       version=version,
@@ -21,7 +21,7 @@ setup(name='Products.jsImagePopups',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
       include_package_data=True,
-      zip_safe=True,
+      zip_safe=False,
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
